81dba5cfd7b7cecb5be7ffc971ce979ca6c925f2
Derrick Hubbard <derrickhubb@gmail.com>
2018-08-15 13:03:49 -0400

    Step2

--- DailyRateUsing/Program.cs
+++ DailyRateUsing/Program.cs
@@ -15,7 +15,26 @@ namespace DailyRate
 
         void run()
         {
-
+            double fee = calculateFee();
+            Console.WriteLine($"Fee is {fee}");
+        }
+        private double calculateFee(double dailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("calculateFee using two optional parameters");
+            return dailyRate * noOfDays;
+        }
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("calculateFee using one optional parameter");
+            int defaultNoOfDays = 1;
+            return dailyRate * defaultNoOfDays;
+        }
+        private double calculateFee()
+        {
+            Console.WriteLine("calculateFee using hardcoded values");
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+            return defaultDailyRate * defaultNoOfDays;
         }
     }
 }
